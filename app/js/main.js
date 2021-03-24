80c131776d70844991a25f28160540336be894ce
yondaime86 <fds090702@mail.ru>
2021-03-24 20:05:55 +0500

    added product content

--- app/js/main.js
+++ app/js/main.js
@@ -25,3 +25,20 @@ $(function(){
     var mixer = mixitup('.products__inner-box');
 
 });
+
+
+$(".tabs-stage .tab").hide();
+$(".tabs-stage .tab:first-child").show();
+$(".tabs-nav li:first").addClass("tab-active");
+
+// Change tab class and display content
+$(".tabs-nav a").on("click", function (event) {
+  event.preventDefault();
+  let parent = $(this).parents(".tabs-container");
+  $(".tabs-nav li" , parent).removeClass("tab-active");
+  $(this)
+    .parent()
+    .addClass("tab-active");
+  $(".tabs-stage .tab", parent).hide();
+  $($(this).attr("href")).show();
+});
